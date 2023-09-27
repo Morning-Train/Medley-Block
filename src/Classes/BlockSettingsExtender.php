6b3c66a22f7b9bd24cbc29e6c1bb6ee21209d547
Mathias Munk <mh@morningtrain.dk>
2023-09-27 10:50:12 +0200

    Use global view function

--- src/Classes/BlockSettingsExtender.php
+++ src/Classes/BlockSettingsExtender.php
@@ -6,16 +6,17 @@ class BlockSettingsExtender
 {
     public function allowViewRenderInBlockMeta(array $settings, array $metadata): array
     {
-        if (! class_exists("\Morningtrain\WP\View\View")) {
-            return $settings;
-        }
 
         if (! isset($metadata['renderView'])) {
             return $settings;
         }
 
         $settings['render_callback'] = static function ($attributes, $content, $block) use ($metadata) {
-            return \Morningtrain\WP\View\View::render($metadata['renderView'], [
+            if (! function_exists('view')) {
+                return '';
+            }
+
+            return view($metadata['renderView'], [
                 'attributes' => $attributes,
                 'content' => $content,
                 'block' => $block,
