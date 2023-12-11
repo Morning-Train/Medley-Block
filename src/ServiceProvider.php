57308fe1f987b59c48d20e4d920190743b3c7342
Mathias Munk <mh@morningtrain.dk>
2023-12-11 15:44:44 +0100

    Fix wrong proudction check

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -9,17 +9,18 @@ use MorningMedley\Block\Classes\Block;
 use Symfony\Component\Cache\Adapter\AbstractAdapter;
 use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
 use MorningMedley\Facades\Block as BlockFacade;
+use function MorningMedley\Functions\config;
 
 class ServiceProvider extends IlluminateServiceProvider
 {
     public function register(): void
     {
+        $this->mergeConfigFrom(__DIR__ . "/config/config.php", 'block');
         BlockFacade::setFacadeApplication($this->app);
         $this->app->singleton('block',
             fn($container) => new Block($container, new BlockRegistrator(),
                 $this->app->make('file.cache', ['namespace' => 'block', 'defaultLifetime' => DAY_IN_SECONDS])));
 
-        $this->mergeConfigFrom(__DIR__ . "/config/config.php", 'block');
     }
 
     public function boot(): void
